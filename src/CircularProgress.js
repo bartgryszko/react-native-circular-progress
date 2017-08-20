c0746e7b88f185b3c81f3e24342152d6e7cb5ec6
Stanimir Marinov <s.marinov@structmedia.com>
2017-08-20 09:01:29 +0300

    remove fix for Android

--- src/CircularProgress.js
+++ src/CircularProgress.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, Platform } from 'react-native';
+import { View } from 'react-native';
 import { Surface, Shape, Path, Group, Filter } from '../../react-native/Libraries/ART/ReactNativeART';
 import MetricsPath from 'art/metrics/path';
 
@@ -9,16 +9,9 @@ export default class CircularProgress extends React.Component {
   circlePath(cx, cy, r, startDegree, endDegree) {
 
     let p = Path();
-    if (Platform.OS === 'ios') {
-      p.path.push(0, cx + r, cy);
-      p.path.push(4, cx, cy, r, startDegree * Math.PI / 180, endDegree * Math.PI / 180, 1);
-    } else {
-      // For Android we have to resort to drawing low-level Path primitives, as ART does not support 
-      // arbitrary circle segments. It also does not support strokeDash.
-      // Furthermore, the ART implementation seems to be buggy/different than the iOS one.
-      // MoveTo is not needed on Android 
-      p.path.push(4, cx, cy, r, startDegree * Math.PI / 180, (startDegree - endDegree) * Math.PI / 180, 0);
-    }
+    p.path.push(0, cx + r, cy);
+    p.path.push(4, cx, cy, r, startDegree * Math.PI / 180, endDegree * Math.PI / 180, 1);
+
     return p;
   }
 
