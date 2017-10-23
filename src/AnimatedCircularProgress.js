adcb434f0197ebc24df85c24c63ff7def20301d9
gamingumar <umaraamer@gmail.com>
2017-10-23 13:11:16 +0500

    bug fix, package json updated.

--- src/AnimatedCircularProgress.js
+++ src/AnimatedCircularProgress.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import ViewPropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { AppState, View, Animated, ViewPropTypes } from 'react-native';
 import CircularProgress from '../node_modules/react-native-circular-progress/src/CircularProgress';
 
