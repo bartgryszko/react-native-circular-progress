53e6ec728533066230af68dc3d04a08381cd84f2
mars-lan <mars.th.lan@gmail.com>
2017-11-04 18:35:28 -0700

    Use the proper PropType for styles

--- src/CircularProgress.js
+++ src/CircularProgress.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, Platform } from 'react-native';
+import { View, Platform, StyleSheet } from 'react-native';
 import { Surface, Shape, Path, Group } from '../../react-native/Libraries/ART/ReactNativeART';
 import MetricsPath from 'art/metrics/path';
 
@@ -55,7 +55,7 @@ export default class CircularProgress extends React.Component {
 }
 
 CircularProgress.propTypes = {
-  style: View.propTypes.style,
+  style: PropTypes.instanceOf(StyleSheet.Styles),
   size: PropTypes.number.isRequired,
   fill: PropTypes.number.isRequired,
   width: PropTypes.number.isRequired,
