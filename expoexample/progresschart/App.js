c3fc5e7a6186ef32d44abb26a81d3be9a7091ef3
Markus Lindqvist <markus.lindqvist@reaktor.com>
2017-11-20 14:34:38 +0200

    Bump version and update example app

---
+++ expoexample/progresschart/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import ProgressChart from './ProgressChart';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <ProgressChart />
+    );
+  }
+}
---
+++ expoexample/progresschart/README.md
@@ -0,0 +1,1 @@
+npm install & Run using the Expo XDE.
